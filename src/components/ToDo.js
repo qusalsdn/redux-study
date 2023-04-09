fe76055cc17871cdf2b6970308bdd9495a42a423
변민우 <yja05092@gmail.com>
2023-04-10 00:07:27 +0900

    Detail Screen

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,12 @@
 import { useDispatch } from "react-redux";
 import { deleteToDo } from "../store";
+import { Link } from "react-router-dom";
 
 function ToDo({ text, id }) {
   const dispatch = useDispatch();
   return (
     <li>
-      {text}
+      <Link to={`/${id}`}>{text}</Link>
       <button onClick={() => dispatch(deleteToDo(id))}>DEL</button>
     </li>
   );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,18 @@
-export default () => {
-  return "Detail";
-};
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const params = useParams();
+  const toDos = useSelector((state) => state);
+  const detailTodo = toDos.find((toDo) => toDo.id === parseInt(params.id));
+
+  return (
+    <>
+      <h1>{detailTodo?.text}</h1>
+      <h5>Created at: {detailTodo.id}</h5>
+      <button>DEL</button>
+    </>
+  );
+}
+
+export default Detail;
