67ef57db1c8520d74de7f79fddc8c72223774015
변민우 <yja05092@gmail.com>
2023-04-10 16:07:33 +0900

    createAction
    redux toolkit의 createAction을 이용하면 action을 정의하지 않아도 된다.

--- src/store.js
+++ src/store.js
@@ -1,28 +1,33 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-export const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// export const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
 
-export const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+// export const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id,
+//   };
+// };
+
+// redux toolkit의 createAction을 이용하면 action을 정의하지 않아도 된다.
+export const addToDo = createAction("ADD");
+export const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
@@ -30,9 +35,9 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo,
+// };
 
 export default store;
